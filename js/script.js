3e85c5c6840c24df1e6fdc51e2bf3c0f703c3193
Alok730 <101257983+Alok730@users.noreply.github.com>
2022-06-30 04:16:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+{/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous"></script> */}
+
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Programmer","", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
